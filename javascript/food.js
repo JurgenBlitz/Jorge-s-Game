5143f83be423366a621609c0d918f7c0b13491f0
JurgenBlitz <jorge.gopae@gmail.com>
2018-01-23 15:11:24 +0100

    Food functions began

--- javascript/food.js
+++ javascript/food.js
@@ -1,25 +1,14 @@
-/* var foods = function () {
-        this.speed = speed;
-        this.x =  Math.random() * (gameView.width) -180;
+ var Food = function () {
+        this.speed = 50;
+        this.x =  Math.random() * (gameView.width) -100;
         this.y = -50;
-        this.points = points;
-        this.renderEnemy();
-      }
+        this.points = function(){
+            var healthyFood = new Food(-10);
+            var junkFood = new Food(+10);
+        }
+    }
 
-
-Enemy.prototype.move = function(){
-  this.x += this.speed;
-
-};
-
-
-var healthyFood = new foods() {
-    this.points = +10,
-
-}
-
-var junkFood = new foods(){
-    this.points = -10,
-}
-    
-*/
+    Food.prototype.renderFood = function(){
+    console.log("Food comes in");
+    ctx.fillRect(this.x, this.y, 50, 50);
+    }
