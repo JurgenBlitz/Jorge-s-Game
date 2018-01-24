c41241b42371c5bb127b00c0a3d824f3c9b071c1
JurgenBlitz <jorge.gopae@gmail.com>
2018-01-24 12:41:23 +0100

    Started to define collisions

--- javascript/functionality.js
+++ javascript/functionality.js
@@ -19,26 +19,40 @@ function Game() {
     this.player.keyboardEventDown.bind(this.player)
   );
 }
-
+//establish both food types
 Game.prototype.generateFood = function() {
   var healthy = Math.random() > 0.5;
   var foodInst;
-  
+
   if (healthy) {
-  	foodInst = new Food(10);
+    foodInst = new Food(10);
   } else {
-  	foodInst = new Food(-10);
+    foodInst = new Food(-10);
   }
   this.food.push(foodInst);
-}
+};
+
+Game.prototype.checkCollision = function(player, food) {
+  if (
+    player.x < food.x + food.width &&
+    player.x + player.width > food.x &&
+    player.y < food.y + food.height &&
+    player.y + player.height > food.y
+  ) {
+  }
+  console.log("Collision");
+};
 
 function renderGame() {
+  //render the player
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   game.player.renderPlayer();
-  
+
+
   //render every food item
   game.food.forEach(function(elem) {
     elem.move();
     elem.render();
   });
+  game.checkCollision();
 }
